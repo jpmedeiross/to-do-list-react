ffb2c8d943c6d387d62c0de9001c49d21ce64ef5
João Pedro <jpmdrsdev@gmail.com>
2024-05-03 13:04:57 -0300

    structuring layout

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -2,9 +2,47 @@ import { useState } from 'react'
 
 function App() {
 
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+
+    {
+      id: 3,
+      text: "Estudar react",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
+
   return (
-    <div>
-      Olá REACT!
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className='category'>({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>Deletar</button>
+            </div>
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
