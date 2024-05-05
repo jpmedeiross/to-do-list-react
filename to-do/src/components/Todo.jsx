c4c7ce91bd861bee2329dce35ba5387ef5a62eb7
Joao Pedro <joaopedroo16@hotmail.com>
2024-05-05 11:12:22 -0300

    adding css to buttons

--- to-do/src/components/Todo.jsx
+++ to-do/src/components/Todo.jsx
@@ -8,8 +8,8 @@ const Todo = ({ todo }) => {
               <p className='category'>({todo.category})</p>
             </div>
             <div>
-              <button>Completar</button>
-              <button>X</button>
+              <button className='complete'>Completar</button>
+              <button className='remove'>x</button>
             </div>
           </div>
   )
